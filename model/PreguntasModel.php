87a4e96af0f104c3087f06ebfe81f80dbfaf2bf0
Paiva <85632116+Paibass@users.noreply.github.com>
2024-06-03 18:21:08 -0300

    Update PreguntasModel.php

--- model/PreguntasModel.php
+++ model/PreguntasModel.php
@@ -9,10 +9,28 @@ class PreguntasModel {
 
     public function getPregunta()
     {
-        $preguntax = $this->database->query("SELECT id AS pregunta_id, texto AS pregunta_texto FROM preguntas WHERE id = 2");
-        $respuestas = $this->database->query("SELECT id AS respuesta_id, texto AS respuesta_texto, es_correcto FROM respuestas WHERE pregunta_id = 2");
-        $preguntax['respuestas'] = $respuestas;
-         return $preguntax;
+        $preguntas = array();
+        //$preguntax = $this->database->query("SELECT id AS pregunta_id, texto AS pregunta_texto FROM preguntas WHERE id = 2");
+        //$respuestas = $this->database->query("SELECT id AS respuesta_id, texto AS respuesta_texto, es_correcto FROM respuestas WHERE pregunta_id = 2");
+        //$preguntax['respuestas'] = $respuestas;
+         //return $preguntax;
+
+        $preguntas_query = $this->database->query("SELECT id AS pregunta_id, texto AS pregunta_texto FROM preguntas ORDER BY RAND() LIMIT 10");
+        while($pregunta = $preguntas_query->fetch_assoc()){
+            $pregunta_id = $pregunta['pregunta_id'];
+
+            $respuestas_query = $this->database->query("SELECT id AS respuestas_id, texto AS respuesta_texto, es_correcto FROM respuestas WHERE pregunta_id = $pregunta_id); 
+            $respuestas = array();
+
+            while($respuesta = $respuestas_query->fetch_assoc()) {
+            $respuestas[] = $respuesta;
+            }
+
+            $pregunta['respuestas'] = $respuestas;
+
+            $preguntas[] = $pregunta;
+        }
+        return $preguntas;
     }
 
 
